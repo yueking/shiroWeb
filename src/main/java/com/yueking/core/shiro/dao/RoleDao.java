46801594eebc53dbafeaa3a7405dba28cca92276
yueking <yuekinger@gmail.com>
2019-07-19 19:20:58 +0800

    prue spring-mvc shiro

--- src/main/java/com/yueking/core/shiro/dao/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.yueking.core.shiro.dao;
-
-import com.yueking.core.shiro.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleDao extends JpaRepository<Role,Long> {
-}
