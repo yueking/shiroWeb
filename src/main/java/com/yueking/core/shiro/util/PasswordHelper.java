903a509f5debea2042bfe5554638387163011488
yueking <yuekinger@gmail.com>
2019-07-19 16:49:31 +0800

    spring shiro

--- src/main/java/com/yueking/core/shiro/util/PasswordHelper.java
+++ src/main/java/com/yueking/core/shiro/util/PasswordHelper.java
@@ -1,17 +1,19 @@
 package com.yueking.core.shiro.util;
 
 import com.yueking.core.shiro.entity.User;
+import lombok.Data;
 import org.apache.shiro.crypto.RandomNumberGenerator;
 import org.apache.shiro.crypto.SecureRandomNumberGenerator;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.util.ByteSource;
 import org.springframework.stereotype.Service;
 
+@Data
 @Service
 public class PasswordHelper {
     private RandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
     private String algorithmName = "md5";
-    private final int hashIterations = 2;
+    private int hashIterations = 2;
 
     public void encryptPassword(User user) {
         user.setSalt(randomNumberGenerator.nextBytes().toHex());
