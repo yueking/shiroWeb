510f7c7a0d801fa05e31e73e7dc4248476b07adb
yueking <yuekinger@gmail.com>
2019-07-20 12:33:09 +0800

    shiro error not impl

--- src/main/java/com/yueking/core/login/LoginController.java
+++ src/main/java/com/yueking/core/login/LoginController.java
@@ -3,11 +3,14 @@ package com.yueking.core.login;
 import com.yueking.core.shiro.model.User;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
@@ -27,8 +30,22 @@ public class LoginController {
     }
 
     @RequestMapping("subLogin")
-    public String subLogin(HttpServletRequest request,User user){
+    public String subLogin(HttpServletRequest request, User user) throws Exception {
         System.out.println("====subLogin"+user);
+
+        String exceptionClassName = (String) request.getAttribute("shiroLoginFailure");
+        if (exceptionClassName != null) {
+            if (exceptionClassName.equals(UnknownAccountException.class.getName())) {
+                //todo throw Exception("用户不存在");
+                throw new Exception("用户不存在");
+            } else if (exceptionClassName.equals(IncorrectCredentialsException.class.getName())) {
+                //todo throw Exception("用户名/密码错误");
+                throw new Exception("用户名/密码错误");
+            } else {
+                throw new Exception();
+            }
+        }
+
         /*获得主体*/
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken taken = new UsernamePasswordToken(user.getUsername(),user.getPassword());
