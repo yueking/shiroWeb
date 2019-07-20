c9f6c68349af65e28aafc4b779b923235bd48062
yueking <yuekinger@gmail.com>
2019-07-20 10:32:02 +0800

    prue spring-mvc

--- src/main/java/com/yueking/core/login/LoginController.java
+++ src/main/java/com/yueking/core/login/LoginController.java
@@ -1,7 +1,12 @@
 package com.yueking.core.login;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -15,8 +20,21 @@ public class LoginController {
         return "login";
     }
 
-    @RequestMapping("intoHome")
-    public String intoHome(){
-        return "home";
+    @RequestMapping("subLogin")
+    public String subLogin(){
+        System.out.println("subLogin");
+        /*获得主体*/
+//        Subject subject = SecurityUtils.getSubject();
+//        UsernamePasswordToken taken = new UsernamePasswordToken(user.getUsername(),user.getPassword());
+//
+//        try {
+//            subject.login(taken);
+//        } catch (AuthenticationException e) {
+//            //e.printStackTrace();
+//            return e.getMessage();
+//        }
+
+        return "登录成功";
+
     }
 }
